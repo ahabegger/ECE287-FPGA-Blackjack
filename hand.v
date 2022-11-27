d1651bb35336cc3e1eb2d51201cf5b06eacfe704
habeggaj <habeggaj@miamioh.edu>
2022-11-27 12:56:31 -0500

    Connect Round to draw

--- hand.v
+++ hand.v
@@ -2,21 +2,13 @@ module hand(
 input [7:0]card_1,
 input [7:0]card_2,
 output reg [7:0]value1,
-output reg [7:0]value2, 
-output reg [7:0]value3,
-output reg [7:0]value4, 
-output reg [7:0]value5,
-output reg [7:0]value6
+output reg [7:0]value2
 );
 
 always@(*)
 begin 
 	value1 = card_1 + card_2; 
 	value2 = card_1 + card_2;
-	value3 = card_1 + card_2; 
-	value4 = card_1 + card_2;
-	value5 = card_1 + card_2; 
-	value6 = card_1 + card_2;
 
 	if ((card_1 == 8'd1) + (card_2 == 8'd1))
 		value1 = value1 + 8'd10;
--- round.v
+++ round.v
@@ -1,5 +1,5 @@
 module round(
-input round_start
+input round_start,
 input [7:0]seed
 );
 
@@ -14,20 +14,20 @@ wire [1:0]suit3;
 wire [1:0]suit4;
 
 wire [7:0]card1;
-wire {7:0}card2;
+wire [7:0]card2;
 wire [7:0]card3;
-wire {7:0}card4;
+wire [7:0]card4;
 
 // random_generator_4 RAND(seed, random1, random2, random3, random5);
 
 
 // DEALER
-draw DCARD1();
-draw DCARD2();
+draw DCARD1(random1, card1, suit1);
+draw DCARD2(random2, card2, suit2);
 
 // PLAYER
-draw PCARD1();
-draw PCARD2();
+draw PCARD1(random3, card3, suit3);
+draw PCARD2(random4, card4, suit4);
 
 // Calculate Hands
 
