0efd8e21698fd7ca1855e0650893f1276c5af88a
habeggaj <habeggaj@miamioh.edu>
2022-11-27 12:25:23 -0500

    Added Draw Module

--- draw.v
+++ draw.v
@@ -1,13 +1,25 @@
 module draw(
-input [7:0]seed,
+input [7:0]random,
 output [7:0]card_code,
 output [1:0]suit_code
 );
+
+// Holder Integer
+integer card_value;
+integer suit_value;
 
 /*
 This module will generate a random card 
 */
 
+always@(*)
+begin 
 
+	card_value = (random % 13) + 1; 
+	suit_code = (random[7:5] % 4) + 1;
 
+	card_code = card_value;
+	suit_code = suit_value;
+	
+end
 endmodule
--- hand.v
+++ hand.v
@@ -1,12 +1,12 @@
 module hand(
 input [7:0]card_1,
 input [7:0]card_2,
-output [7:0]value1,
-output [7:0]value2, 
-output [7:0]value3,
-output [7:0]value4, 
-output [7:0]value5,
-output [7:0]value6
+output reg [7:0]value1,
+output reg [7:0]value2, 
+output reg [7:0]value3,
+output reg [7:0]value4, 
+output reg [7:0]value5,
+output reg [7:0]value6
 );
 
 always@(*)
@@ -18,7 +18,7 @@ begin
 	value5 = card_1 + card_2; 
 	value6 = card_1 + card_2;
 
-	if (card_1 == 8'd1 or card_2 == 8'd1)
+	if ((card_1 == 8'd1) + (card_2 == 8'd1))
 		value1 = value1 + 8'd10;
 
 end
