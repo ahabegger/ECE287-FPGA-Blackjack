58020765c3a8ecbd103a8f28ef6e3b172eae3ac9
habeggaj <habeggaj@miamioh.edu>
2022-11-27 12:06:57 -0500

    Added hand module

---
+++ card.v
@@ -0,0 +1,17 @@
+module hand(
+input [4:0]card_1,
+input [4:0]card_2,
+output [3:0]value1,
+output [3:0]value2
+);
+
+always@(*)
+begin 
+	case(card_code)
+
+	endcase
+
+
+end
+
+endmodule
---
+++ hand.v
@@ -0,0 +1,26 @@
+module hand(
+input [7:0]card_1,
+input [7:0]card_2,
+output [7:0]value1,
+output [7:0]value2, 
+output [7:0]value3,
+output [7:0]value4, 
+output [7:0]value5,
+output [7:0]value6
+);
+
+always@(*)
+begin 
+	value1 = card_1 + card_2; 
+	value2 = card_1 + card_2;
+	value3 = card_1 + card_2; 
+	value4 = card_1 + card_2;
+	value5 = card_1 + card_2; 
+	value6 = card_1 + card_2;
+
+	if (card_1 == 8'd1 or card_2 == 8'd1)
+		value1 = value1 + 8'd10;
+
+end
+
+endmodule
