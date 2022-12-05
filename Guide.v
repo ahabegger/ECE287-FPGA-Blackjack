ed8c37a219c53728009c432db8f87f13259a277f
Jrhawkins6 <119711095+Jrhawkins6@users.noreply.github.com>
2022-12-05 17:16:57 -0500

    Update Guide.v

--- Guide.v
+++ Guide.v
@@ -1,36 +1,34 @@
-module Guide (	//	Host Side
+module Guide (	
 						iDATA,iRS,
 						iStart,oDone,
 						iCLK,iRST_N,
-						//	LCD Interface
 						LCD_DATA,
 						LCD_RW,
 						LCD_EN,
 						LCD_RS	);
-//	CLK
+
 parameter	CLK_Divide	=	16;
 
-//	Host Side
+
 input	[7:0]	iDATA;
 input	iRS,iStart;
 input	iCLK,iRST_N;
 output	reg		oDone;
-//	LCD Interface
+
 output	[7:0]	LCD_DATA;
 output	reg		LCD_EN;
 output			LCD_RW;
 output			LCD_RS;
-//	Internal Register
+
 reg		[4:0]	Cont;
 reg		[1:0]	ST;
 reg		preStart,mStart;
 
-/////////////////////////////////////////////
-//	Only write to LCD, bypass iRS to LCD_RS
-assign	LCD_DATA	=	iDATA; 
-assign	LCD_RW		=	1'b0;
-assign	LCD_RS		=	iRS;
-/////////////////////////////////////////////
+
+assign	LCD_DATA = iDATA; 
+assign	LCD_RW = 1'b0;
+assign	LCD_RS =iRS;
+
 
 always@(posedge iCLK or negedge iRST_N)
 begin
@@ -45,21 +43,19 @@ begin
 	end
 	else
 	begin
-		//////	Input Start Detect ///////
 		preStart <=	iStart;
 		if({preStart,iStart}==2'b01)
 		begin
 			mStart	<=	1'b1;
 			oDone	<=	1'b0;
 		end
-		//////////////////////////////////
 		if(mStart)
 		begin
 			case(ST)
-			0:	ST	<=	1;	//	Wait Setup
+			0:	ST <= 1;
 			1:	begin
-					LCD_EN	<=	1'b1;
-					ST		<=	2;
+					LCD_EN <= 1'b1;
+					ST <= 2;
 				end
 			2:	begin					
 					if(Cont < CLK_Divide)
@@ -68,10 +64,10 @@ begin
 						ST <= 3;
 				end
 			3:	begin
-					LCD_EN	<=	1'b0;
-					mStart	<=	1'b0;
-					oDone	<=	1'b1;
-					Cont	<=	0;
+					LCD_EN <= 1'b0;
+					mStart <= 1'b0;
+					oDone <= 1'b1;
+					Cont <= 0;
 					ST		<=	0;
 				end
 			endcase
